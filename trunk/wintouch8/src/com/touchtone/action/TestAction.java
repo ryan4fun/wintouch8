e3380cad457cd9814fef7be1454d2d25cdc892c9
coderrr.cn@gmail.com <coderrr.cn@gmail.com@0e193955-2a2b-2581-5c3e-e57a02387481>
2012-10-21 04:26:31 +0000

    Initialize project.

---
+++ trunk/wintouch8/src/com/touchtone/action/TestAction.java
@@ -0,0 +1,37 @@
+package com.touchtone.action;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.softwarementors.extjs.djn.config.annotations.DirectMethod;
+
+public class TestAction {
+	@DirectMethod
+	public DataStore getGrid(Map params) {
+		
+		DataStore store = new DataStore();
+		
+		List<Map<String, String>> records = new ArrayList<Map<String, String>>();
+		
+		Map<String, String> record1 = new HashMap<String, String>();
+		record1.put("name", "Yuanyuan");
+		record1.put("turnover", "123");
+		records.add(record1);
+		
+		Map<String, String> record2 = new HashMap<String, String>();
+		record2.put("name", "Leon");
+		record2.put("turnover", "456");
+		records.add(record2);
+		
+		Map<String, String> record3 = new HashMap<String, String>();
+		record3.put("name", "Naya");
+		record3.put("turnover", "789");
+		records.add(record3);
+		
+		store.setTotalCount(records.size());
+		store.setRecords(records);
+		return store;
+	}
+}
